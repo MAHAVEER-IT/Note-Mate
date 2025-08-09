30ae35b2f0388820f94e41e8e94caecf5ab23ca0
Mahaveer K <mahaveer.k2023it@sece.ac.in>
2025-08-09 22:46:17 +0530

    bug fixed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './LandingPage.css';
 import noteMateImg from '../../assets/images/Notes.png';
+import './LandingPage.css';
 
 const LandingPage = () => {
   const navigate = useNavigate();
